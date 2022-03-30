24efa8171a6833ae2327e801c8c6a1160d03c4a1
singlegray <50613216+singlegray@users.noreply.github.com>
2022-03-30 10:23:23 +0300

    add

--- lib/question.rb
+++ lib/question.rb
@@ -1,9 +1,32 @@
 class Question
-  attr_reader :question, :answer, :score
+  attr_reader :question, :answer, :points, :minutes, :variants
 
-  def initialize(question, answer, score, minutes)
+  def self.from_file
+    questions = []
+
+    filename = File.join(__dir__,'/../', 'data', 'questions.xml')
+    file = File.new(filename)
+
+    doc = REXML::Document.new(file)
+    doc.elements.each("questions/question") do |item|
+      minutes = item.attributes["minutes"]
+      points = item.attributes["points"]
+      text = item.elements["text"].first
+      variants = []
+      item.elements.each("variants/variant") do |variant|
+        binding.irb
+        variants << variant.elements["variant"].first
+        
+        answer = variant.elements["variant"].first if item.attributes["right"] == "true"
+      end
+      questions << self.new(text, answer, points, minutes, variants)
+    end
+    return questions
+  end
+
+  def initialize(question, answer, points, minutes, variants)
     @question = question
     @answer = answer.downcase
-    @score = score.to_i
+    @points = points.to_i
   end
 end
